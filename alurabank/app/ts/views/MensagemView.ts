58895c0c94d20ed66c1475243c66d52fa57030d6
Fernanda Aparecida Ferreira <fernanda.fereira@tegra.com.br>
2019-08-03 22:01:48 -0300

    finish aula 04

---
+++ alurabank/app/ts/views/MensagemView.ts
@@ -0,0 +1,7 @@
+class MensagemView extends View<string> {
+
+    template(modelo: string) {
+        return `<p class="alert alert-info">${modelo}</p>`;
+    }
+
+}
--- alurabank/app/ts/views/NegociacoesView.ts
+++ alurabank/app/ts/views/NegociacoesView.ts
@@ -1,14 +1,4 @@
-class NegociacoesView {
-
-    private _elemento: Element;
-
-    constructor(seletor: string) {
-        this._elemento = document.querySelector(seletor);
-    }
-
-    update(modelo: Negociacoes) {
-        this._elemento.innerHTML = this.template(modelo);
-    }
+class NegociacoesView extends View<Negociacoes> {
 
     template(modelo: Negociacoes): string {
 
